6b137e85cdfa43a87182b11434fdc0993b1b548a
D.Atanasova <mad_n_happyy@abv.bg>
2017-10-30 19:48:48 +0200

    Add files via upload

---
+++ SoftUni Blog C/Models/BlogDbContex.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Models
+{
+    public class BlogDbContex : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContex()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public virtual IDbSet<Article> Articles { get; set; }
+
+        public static BlogDbContex Create()
+        {
+            return new BlogDbContex();
+        }
+    }
+}
